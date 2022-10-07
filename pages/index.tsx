7470246a0a747efbc0a9a623ff42792a427d322e
Josué Almonasi <josue.almonasi@hotmail.com>
2022-10-07 03:32:01 -0500

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
 import { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return (
-    <>
-      <h1>Hello world!</h1>
-    </>
-  );
+  return <h1>Hello world!</h1>;
 };
 
 export default Home;
