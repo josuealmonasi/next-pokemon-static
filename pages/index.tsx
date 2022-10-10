9fb5adbcd2c70b476bed8723e65103765e11e8e3
Josué Almonasi <josue.almonasi@hotmail.com>
2022-10-09 22:38:28 -0500

    Added new page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import { NextPage } from 'next';
-import Layout from './components/layouts';
+import Layout from '../components/layouts';
 
 const Home: FC<NextPage> = () => {
   return (
