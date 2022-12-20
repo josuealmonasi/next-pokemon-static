a4c6686d7260e31539506ad6decae07a73105378
Josué Almonasi <josue.almonasi@hotmail.com>
2022-12-20 16:23:43 -0600

    Added fav redirect to pokemon card

--- components/pokemon/FavPokemonCard.tsx
+++ components/pokemon/FavPokemonCard.tsx
@@ -1,20 +1,31 @@
-import { Grid, Card } from '@nextui-org/react';
 import React, { FC } from 'react';
+import { Grid, Card } from '@nextui-org/react';
+import { useRouter } from 'next/router';
 
 interface FavPokemonCard {
   id: number;
 }
-export const FavPokemonCard: FC<FavPokemonCard> = ({ id }) => (
-  <Grid xs={6} sm={3} xl={1} key={id}>
-    <Card isHoverable>
-      <Card.Body css={{ p: 1 }}>
-        <Card.Image
-          loading='lazy'
-          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
-          width='100%'
-          height={140}
-        />
-      </Card.Body>
-    </Card>
-  </Grid>
-);
+export const FavPokemonCard: FC<FavPokemonCard> = ({ id }) => {
+  const router = useRouter();
+
+  const handleOnclick = async () => {
+    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const { name } = await pokemon.json();
+    router.push(`/pokemon/${name}`);
+  };
+
+  return (
+    <Grid xs={6} sm={3} xl={1} onClick={handleOnclick}>
+      <Card isHoverable isPressable>
+        <Card.Body css={{ p: 1 }}>
+          <Card.Image
+            loading='lazy'
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
+            width='100%'
+            height={140}
+          />
+        </Card.Body>
+      </Card>
+    </Grid>
+  );
+};
